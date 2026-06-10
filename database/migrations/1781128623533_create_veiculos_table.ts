2d2ce50952568725e784d2aca948f03d377fb744
JoaoManoelAlves <joaomanoel88642@gmail.com>
2026-06-10 19:25:49 -0300

    Criacao dos migrations e configuracao do postgres

---
+++ database/migrations/1781128623533_create_veiculos_table.ts
@@ -0,0 +1,22 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'veiculos'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').notNullable()
+      table.string('placa').notNullable().unique()
+      table.string('modelo').notNullable()
+      table.string('marca').notNullable()
+      table.integer('ano').notNullable()
+      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ database/migrations/1781129973043_create_manuntencaos_table.ts
@@ -0,0 +1,26 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'manuntencaos'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').notNullable()
+      table.string('descricao').notNullable()
+      table.date('data').notNullable()
+      table.float('custo').notNullable()
+      table
+        .integer('vehicle_id')
+        .unsigned()
+        .references('id')
+        .inTable('vehicles')
+        .onDelete('CASCADE')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
